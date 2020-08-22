20c03f76351830b12de5e84693cd8c33d6653ceb
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-08-22 19:38:13 -0300

    adicionado hitboxes e hurtboxes

--- Resources/World/Grass.gd
+++ Resources/World/Grass.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_attack"):
-		var GrassEffect = load("res://Resources/Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		get_parent().add_child(grassEffect)
-		grassEffect.global_position = self.position
-		
-		queue_free()
+func create_grass_effect():
+	var GrassEffect = load("res://Resources/Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	get_parent().add_child(grassEffect)
+	grassEffect.global_position = self.position
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
